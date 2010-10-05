3c249c2da7488251a3c2b97522865154e0a67604
Yasser González Fernández <ygonzalezfernandez@gmail.com>
2010-10-04 21:59:16 -0400

    VEDA updated to select an ordering of the variables.

--- R/VEDA.R
+++ R/VEDA.R
@@ -23,45 +23,42 @@ setClass("VEDA",
 learningVEDA <- function(eda, currGen, oldModel, selectedPop, selectedEval) {
   fmargin <- eda@options$fmargin
   pmargin <- eda@options$pmargin
+  orderingMethod <- eda@options$orderingMethod
+  orderingArgs <- eda@options$orderingArgs
+  type <- eda@options$type
   fitMethod <- eda@options$fitMethod
-  vine <- eda@options$vine
-  trees <- eda@options$trees
-  copulas <- eda@options$copulas
-  corTestMethod <- eda@options$corTestMethod
-  corTestSigLevel <- eda@options$corTestSigLevel
-  gofCopulaIters <- eda@options$gofCopulaIters 
-  gofCopulaMethod <- eda@options$gofCopulaMethod 
-  gofCopulaSimul <- eda@options$gofCopulaSimul
-  fitOptimMethod <- eda@options$fitOptimMethod
-  fitOptimControl <- eda@options$fitOptimControl  
+  fitArgs <- eda@options$fitArgs
   
   if (is.null(fmargin)) fmargin <- fnorm
   if (is.null(pmargin)) pmargin <- pnorm
+  if (is.null(orderingMethod)) orderingMethod <- "greedy"
+  if (is.null(orderingArgs)) orderingArgs <- list(according = "kendall")
+  if (is.null(type)) type <- "DVine"
   if (is.null(fitMethod)) fitMethod <- "ml"
-  if (is.null(vine)) vine <- "DVine"
-  if (is.null(trees)) trees <- ncol(selectedPop) - 1
-  if (is.null(copulas)) copulas <- list(normalCopula(0), tCopula(0), 
-        claytonCopula(1), gumbelCopula(1))
-  if (is.null(corTestMethod)) corTestMethod <- "kendall"
-  if (is.null(corTestSigLevel)) corTestSigLevel <- 0.1
-  if (is.null(gofCopulaIters)) gofCopulaIters <- 1000
-  if (is.null(gofCopulaMethod)) gofCopulaMethod <- "itau"
-  if (is.null(gofCopulaSimul)) gofCopulaSimul <- "mult"
-  if (is.null(fitOptimMethod)) fitOptimMethod <- NULL
-  if (is.null(fitOptimControl)) fitOptimControl <- list()
-
+  if (is.null(fitArgs)) fitArgs <- list(
+        trees = ncol(selectedPop) - 1,
+        copulas = list(normalCopula(0), tCopula(0), 
+            claytonCopula(1), gumbelCopula(1)),
+        corTestMethod = "kendall",
+        corTestSigLevel = 0.1,
+        gofCopulaIters = 1000,
+        gofCopulaMethod = "itau",
+        gofCopulaSimul = "mult",
+        optimMethod = NULL,
+        optimControl = list())
+  
   n <- ncol(selectedPop)
   margins <- lapply(seq(length = n), 
       function (i) fmargin(selectedPop[ , i]))
-  U <- sapply(seq(length = n),
+  uniformPop <- sapply(seq(length = n),
       function (i) do.call(pmargin, c(list(selectedPop[ , i]), margins[[i]])))
-  vine <- fitVine(vine, U, method = fitMethod, trees = trees, copulas = copulas, 
-      corTestMethod = corTestMethod, corTestSigLevel = corTestSigLevel,
-      gofCopulaIters = gofCopulaIters, gofCopulaMethod = gofCopulaMethod, 
-      gofCopulaSimul = gofCopulaSimul, optimMethod = fitOptimMethod, 
-      optimControl = fitOptimControl)@vine
+  ordering <- do.call(orderingVine,
+      c(list(type = type, data = uniformPop, method = orderingMethod), orderingArgs))
+  orderedPop <- uniformPop[ , ordering]
+  vine <- do.call(fitVine,
+      c(list(type = type, data = orderedPop, method = fitMethod), fitArgs))@vine
 
-  list(vine = vine, margins = margins)
+  list(margins = margins, ordering = ordering, vine = vine)
 }
 
 setMethod("learning", "VEDA", learningVEDA)
@@ -73,12 +70,17 @@ samplingVEDA <- function (eda, currGen, model, lower, upper) {
   
   if (is.null(popSize)) popSize <- 100
   if (is.null(qmargin)) qmargin <- qnorm
+
+  orderedPop <- rvine(model$vine, popSize)
+  uniformPop <- matrix(NA, nrow(orderedPop), ncol(orderedPop))
+  for (k in seq(length = ncol(orderedPop))) {
+    uniformPop[ , model$ordering[k]] <- orderedPop[ , k]
+  }
+  finalPop <- sapply(seq(length = ncol(uniformPop)),
+      function (i) do.call(qmargin, 
+            c(list(uniformPop[ , i]), model$margins[[i]])))
   
-  U <- rvine(model$vine, popSize)
-  pop <- sapply(seq(length = ncol(U)),
-      function (i) do.call(qmargin, c(list(U[ , i]), model$margins[[i]])))
-  
-  pop
+  finalPop
 }
 
 setMethod("sampling", "VEDA", samplingVEDA)
