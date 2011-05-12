38e6e9fcd7387c8ccf2e97b863de876a62d2d912
Yasser González Fernández <ygonzalezfernandez@gmail.com>
2011-05-11 22:27:32 -0400

    Updated the copyright notices in the source files.

--- R/termination.R
+++ R/termination.R
@@ -1,6 +1,6 @@
 # edas: R package for Estimation of Distribution Algorithms
-# Copyright (C) 2010-2011 Yasser González-Fernández
-# Copyright (C) 2010-2011 Marta Soto
+# Copyright (C) 2010, 2011 Yasser González-Fernández
+# Copyright (C) 2010, 2011 Marta Soto
 #
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software 
