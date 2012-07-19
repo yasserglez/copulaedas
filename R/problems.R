adb5f3760f09a83552e02e78df070d5fd55a94b5
Yasser González Fernández <ygonzalezfernandez@gmail.com>
2012-07-19 13:01:06 -0400

    Using full names in the copyright notice.

--- R/problems.R
+++ R/problems.R
@@ -1,6 +1,6 @@
 # copulaedas: Estimation of Distribution Algorithms Based on Copulas
-# Copyright (C) 2010-2012 Yasser González-Fernández <ygf@icimaf.cu>
-# Copyright (C) 2010-2012 Marta Soto <mrosa@icimaf.cu>
+# Copyright (C) 2010-2012 Yasser González Fernández <ygonzalezfernandez@gmail.com>
+# Copyright (C) 2010-2012 Marta Rosa Soto Ortiz <mrosa@icimaf.cu>
 #
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
